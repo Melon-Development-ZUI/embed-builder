a1646bb389429d8cf7097147f4c5a5ac753e292e
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2022-04-08 00:01:44 +0530

    merged api controller with api routes

--- routes/views.js
+++ routes/views.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const route = express.Router();
 
+const controllers = require('../controllers/api')
+
 route
     .route('/')
+    .post(controllers.send)
 
 route
     .route('/server/:id')
+    .post(controllers.login)
 
 module.exports = route;
