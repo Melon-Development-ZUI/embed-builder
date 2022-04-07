0e57192b11ebcb15ef78b8b1fbfc42b8c8885032
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2022-04-08 00:03:38 +0530

    proper api merge

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const route = express.Router();
 
+const controllers = require('../controllers/views')
+
 route
     .route('/send')
+    .post(controllers.send)
 
 route
     .route('/login')
+    .post(controllers.login)
 
 module.exports = route;
