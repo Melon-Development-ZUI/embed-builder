f456b1ad00c07f3e56fbb232c844a9ba1166af17
Lemon Rose <japandotorg@users.noreply.github.com>
2022-05-21 01:17:23 +0530

    builder config

---
+++ builder.config.js
@@ -0,0 +1,28 @@
+options = {
+    username:           'Discord Bot',
+    avatar:             'https://cdn.discordapp.com/embed/avatars/1.png',
+    verified:           false,
+    noUser:             false,
+    dataSpecified:      false,
+    guiTabs:            ['author', 'description'],
+    useJsonEditor:      false,
+    reverseColumns:     false,
+    allowPlaceholders:  false,
+    autoUpdateURL:      false,
+    autoParams:         false,
+    hideEditor:         false,
+    hidePreview:        false,
+    hideMenu:           false,
+}
+
+// json = {
+//     content: "Hello world",
+//     embed: {
+//         title: "A title",
+//         description: "A description",
+//     }
+// }
+
+onload = () => {
+    console.log('loaded builder');
+}
